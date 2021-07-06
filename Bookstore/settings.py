cc3521d3395e358969ee94e03d1785e5fcd897cc
harsh <sharshp550@gmail.com>
2021-07-06 12:39:01 +0530

    setting.py

--- Bookstore/settings.py
+++ Bookstore/settings.py
@@ -100,9 +100,9 @@ DATABASES = {
     }
 }
 ...
-DATABASE_URL = os.environ['DATABASE_URL']
+#DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
